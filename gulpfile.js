9b3ff8c4a7e1dcb2b7debef472a0c4b919d6247d
Anick Zet <vip.zatsarnyy@gmail.com>
2021-01-15 19:57:58 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+const { src, dest, watch, parallel, series } = require('gulp');
+
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const ttf2woff = require('gulp-ttf2woff');
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/'
+		}
+	});
+};
+
+function cleanDist() {
+	return del('dist')
+
+};
+
+function fonts() {
+  src('app/fonts/*.ttf')
+    .pipe(ttf2woff())
+    .pipe(dest('app/fonts/'))
+  return src('app/fonts/*.ttf')
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts/'));
+};
+
+function images() {
+	return src('app/images/**/*')
+		.pipe(imagemin(
+			[
+				imagemin.gifsicle({ interlaced: true }),
+				imagemin.mozjpeg({ quality: 75, progressive: true }),
+				imagemin.optipng({ optimizationLevel: 5 }),
+				imagemin.svgo({
+					plugins: [
+						{ removeViewBox: true },
+						{ cleanupIDs: false }
+					]
+				})
+			]
+		))
+		.pipe(dest('dist/images'))
+
+};
+
+function scripts() {
+	return src([
+		'node_modules/jquery/dist/jquery.js',
+		'app/js/main.js'
+	])
+		.pipe(concat('main.min.js'))
+		.pipe(uglify())
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+};
+
+
+function styles() {
+	return src('app/scss/style.scss')
+		.pipe(scss({ outputStyle: 'compressed' }))
+		.pipe(concat('style.min.css'))
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 10 version'],
+			grid: true
+		}))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+
+};
+
+function build() {
+	return src([
+		'app/css/style.min.css',
+		'app/fonts/**/*.woff',
+		'app/fonts/**/*.woff2',
+		'app/js/main.min.js',
+		'app/*.html'
+	], { base: 'app' })
+		.pipe(dest('dist'))
+};
+
+function wathing() {
+	watch(['app/scss/**/*.scss'], styles);
+	watch(['app/fonts/**/*.ttf'], fonts);
+	watch(['app/**/*.js', '!app/js/main.min.js'], scripts);
+	watch(['app/*.html']).on('change', browserSync.reload);
+};
+
+exports.styles = styles;
+exports.wathing = wathing;
+exports.fonts = fonts;
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
+
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles, scripts, fonts, browsersync, wathing);
