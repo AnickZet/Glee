e4b87df96b786a200379bca5cc6082ea8da0b67e
Anick Zet <vip.zatsarnyy@gmail.com>
2021-01-28 14:58:41 +0300

    responsiv

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,37 @@ $(function () {
     dots: true
   });
 
+  $(".burger, .menu a").on("click", function () {
+    $(".burger__line").toggleClass("burger__line--active")
+    $(".menu").toggleClass("menu--active")
+  });
+
   $(".partner__inner").slick({
     arrows: false,
     dots: false,
     slidesToShow: 5,
     autoplay: true,
     speed: 300,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      }
+    ]
   });
 
   var containerEl1 = document.querySelector('[data-ref="products"]');
